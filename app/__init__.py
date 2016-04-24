fd37e79ee61a3d0d17b514f7ecff7f19f6c3a2dc
tjgavlick <tjgavlick@gmail.com>
2016-04-24 13:36:11 -0700

    Strip whitespace from jinja template rendering

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 db = SQLAlchemy(app)
 
