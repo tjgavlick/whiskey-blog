f90d0b4e79d60b5d806343cc2c6b45f4bbb69d97
tjgavlick <tjgavlick@gmail.com>
2016-04-23 19:23:18 -0700

    Add distiller management

--- app/views.py
+++ app/views.py
@@ -55,7 +55,7 @@ def admin_edit_drinktype(drink_type_id):
     return render_template('admin_edit_drinktype.html', drink_type=drink_type)
 
 
-@app.route('/admin/save/drinktype/', methods=['POST'])
+@app.route('/admin/drinktype/save/', methods=['POST'])
 def save_drinktype():
     drink_type_id = int(request.form['id'])
     drink_type_name = request.form['name']
@@ -66,20 +66,71 @@ def save_drinktype():
         drink_type.name = drink_type_name
     # if we're adding a new entry
     else:
-        drink_type = DrinkType(request.form['name'])
+        drink_type = DrinkType(drink_type_name)
 
     db.session.add(drink_type)
     db.session.commit()
     return redirect(url_for('admin_list_drinktypes'))
 
 
-@app.route('/admin/remove/drinktype/<int:drink_type_id>')
+@app.route('/admin/drinktype/remove/<int:drink_type_id>')
 def admin_remove_drinktype(drink_type_id):
     drink_type = DrinkType.query.filter_by(id=int(drink_type_id)).first()
     if not drink_type is None:
         db.session.delete(drink_type)
         db.session.commit()
     return redirect(url_for('admin_list_drinktypes'))
+
+
+# admin - distillers
+####################
+
+@app.route('/admin/distiller/')
+def admin_list_distillers():
+    distillers = Distiller.query.all()
+    return render_template('admin_list_distillers.html', distillers=distillers)
+
+
+@app.route('/admin/distiller/new/')
+def admin_new_distiller():
+    origins = Origin.query.all()
+    return render_template('admin_edit_distiller.html', distiller=None, origins=origins)
+
+
+@app.route('/admin/distiller/<int:distiller_id>')
+def admin_edit_distiller(distiller_id):
+    distiller = Distiller.query.filter_by(id=distiller_id).one()
+    origins = Origin.query.all()
+    return render_template('admin_edit_distiller.html', distiller=distiller, origins=origins)
+
+
+@app.route('/admin/distiller/save/', methods=['POST'])
+def save_distiller():
+    distiller_id = int(request.form['id'])
+    distiller_name = request.form['name']
+    distiller_origin = int(request.form['origin'])
+
+    # if we're editing an existing entry
+    if distiller_id > 0:
+        distiller = Distiller.query.filter_by(id=distiller_id).one()
+        distiller.name = distiller_name
+        distiller.origin_id = distiller_origin
+    # if we're adding a new entry
+    else:
+        distiller = Distiller(distiller_name, distiller_origin)
+
+    db.session.add(distiller)
+    db.session.commit()
+    return redirect(url_for('admin_list_distillers'))
+
+
+@app.route('/admin/distiller/remove/<int:distiller_id>')
+def admin_remove_distiller(distiller_id):
+    distiller = Distiller.query.filter_by(id=int(distiller_id)).first()
+    if not distiller is None:
+        db.session.delete(distiller)
+        db.session.commit()
+    return redirect(url_for('admin_list_distillers'))
 
 
 # admin - origins
@@ -102,7 +153,7 @@ def admin_edit_origin(origin_id):
     return render_template('admin_edit_origin.html', origin=origin)
 
 
-@app.route('/admin/save/origin/', methods=['POST'])
+@app.route('/admin/origin/save/', methods=['POST'])
 def save_origin():
     origin_id = int(request.form['id'])
     origin_name = request.form['name']
@@ -113,14 +164,14 @@ def save_origin():
         origin.name = origin_name
     # if we're adding a new entry
     else:
-        origin = Origin(request.form['name'])
+        origin = Origin(origin_name)
 
     db.session.add(origin)
     db.session.commit()
     return redirect(url_for('admin_list_origins'))
 
 
-@app.route('/admin/remove/origin/<int:origin_id>')
+@app.route('/admin/origin/remove/<int:origin_id>')
 def admin_remove_origin(origin_id):
     origin = Origin.query.filter_by(id=int(origin_id)).first()
     if not origin is None:
