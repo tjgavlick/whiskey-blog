0cfcaeff08758928a1924ea7731df995f5b7bf37
tjgavlick <tjgavlick@gmail.com>
2016-04-23 18:30:07 -0700

    Add origin management

--- app/views.py
+++ app/views.py
@@ -49,7 +49,7 @@ def admin_new_drinktype():
     return render_template('admin_edit_drinktype.html', drink_type=None)
 
 
-@app.route('/admin/drinktype/<drink_type_id>')
+@app.route('/admin/drinktype/<int:drink_type_id>')
 def admin_edit_drinktype(drink_type_id):
     drink_type = DrinkType.query.filter_by(id=drink_type_id).one()
     return render_template('admin_edit_drinktype.html', drink_type=drink_type)
@@ -73,10 +73,57 @@ def save_drinktype():
     return redirect(url_for('admin_list_drinktypes'))
 
 
-@app.route('/admin/remove/drinktype/<drink_type_id>')
+@app.route('/admin/remove/drinktype/<int:drink_type_id>')
 def admin_remove_drinktype(drink_type_id):
     drink_type = DrinkType.query.filter_by(id=int(drink_type_id)).first()
     if not drink_type is None:
         db.session.delete(drink_type)
         db.session.commit()
     return redirect(url_for('admin_list_drinktypes'))
+
+
+# admin - origins
+#################
+
+@app.route('/admin/origin/')
+def admin_list_origins():
+    origins = Origin.query.all()
+    return render_template('admin_list_origins.html', origins=origins)
+
+
+@app.route('/admin/origin/new/')
+def admin_new_origin():
+    return render_template('admin_edit_origin.html', origin=None)
+
+
+@app.route('/admin/origin/<int:origin_id>')
+def admin_edit_origin(origin_id):
+    origin = Origin.query.filter_by(id=origin_id).one()
+    return render_template('admin_edit_origin.html', origin=origin)
+
+
+@app.route('/admin/save/origin/', methods=['POST'])
+def save_origin():
+    origin_id = int(request.form['id'])
+    origin_name = request.form['name']
+
+    # if we're editing an existing entry
+    if origin_id > 0:
+        origin = Origin.query.filter_by(id=origin_id).one()
+        origin.name = origin_name
+    # if we're adding a new entry
+    else:
+        origin = Origin(request.form['name'])
+
+    db.session.add(origin)
+    db.session.commit()
+    return redirect(url_for('admin_list_origins'))
+
+
+@app.route('/admin/remove/origin/<int:origin_id>')
+def admin_remove_origin(origin_id):
+    origin = Origin.query.filter_by(id=int(origin_id)).first()
+    if not origin is None:
+        db.session.delete(origin)
+        db.session.commit()
+    return redirect(url_for('admin_list_origins'))
