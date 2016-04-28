571b27b9afaf1e4b98fe59b5e8dfbd9fce7e4eed
tjgavlick <tjgavlick@gmail.com>
2016-04-28 15:54:52 -0700

    Uncomment postgres database uri

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 class Config(object):
     DEBUG = False
     # db
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://user:pass@localhost/dbname'
     SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     # file upload
     UPLOAD_FOLDER = 'app/static/uploads/'
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://tdw:tdw@localhost/tdw'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://tdw:tdw@localhost/tdw'
