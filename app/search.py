376285c6bc457a3add5d9a673d12375b9d23c952
tjgavlick <tjgavlick@gmail.com>
2016-05-12 08:41:22 -0700

    Query a document using build-in postgres leximes

--- app/search.py
+++ app/search.py
@@ -10,16 +10,21 @@ from app import app, db
 @app.route('/search/')
 def search():
 
-    # compile each review into a text-string 'document'
-    tmp = db.session.execute("SELECT to_tsvector(review.title) || \
-                                     to_tsvector(review.subtitle) || \
-                                     to_tsvector(distiller.name) || \
-                                     to_tsvector(review.body) \
-                                     AS document \
-                              FROM review \
-                              LEFT JOIN distiller ON distiller.id = review.distiller_id \
-                              WHERE review.is_published \
-                              GROUP BY review.id, distiller.id;")
+    # hoo boy. compile reviews into full-text 'documents' and search on them
+    tmp = db.session.execute("SELECT r_id, r_title, r_subtitle FROM ( \
+                                  SELECT review.id AS r_id, \
+                                         review.title AS r_title, \
+                                         review.subtitle AS r_subtitle, \
+                                         to_tsvector(review.title) || \
+                                         to_tsvector(review.subtitle) || \
+                                         to_tsvector(distiller.name) || \
+                                         to_tsvector(review.body) \
+                                         AS document \
+                                  FROM review \
+                                  LEFT JOIN distiller ON distiller.id = review.distiller_id \
+                                  WHERE review.is_published \
+                                  GROUP BY review.id, distiller.id) article_search \
+                              WHERE article_search.document @@ to_tsquery('craig');")
     for row in tmp:
         print(row)
 
